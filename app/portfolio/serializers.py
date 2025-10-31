da08ca17248eebf0359a5939ad048985a77f6124
Max Eriksson <teratoscincus@hotmail.com>
2025-10-31 18:14:37 +0100

    feat(portfolio): serialize model

---
+++ app/portfolio/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from .models import Project
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = (
+            "id",
+            "title",
+            "repo_url",
+            "live_url",
+            "summary",
+            "description",
+            "slug",
+        )
