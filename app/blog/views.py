b05d5b2cd9c6afbc78bd4cc580629dff0f0dfda2
Max Eriksson <teratoscincus@hotmail.com>
2025-11-09 21:08:13 +0100

    feat(blog): migrate views to REST API

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,52 +1,36 @@
+from blog.serializers import BlogPostSerializer
 from django.http import Http404
 from django.shortcuts import get_object_or_404
-from django.views.generic import DetailView, ListView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from taggit.models import Tag
 
-from .models import BlogPost, BlogPostParagraph, BlogPostSnippet
+from .models import BlogPost
 
 
-class BlogPostListView(ListView):
+class BlogPostListView(APIView):
     """Overview of all blog posts."""
+
+    def get(self, request, tag=None):
+        if tag:
+            tag = get_object_or_404(Tag, slug=tag)
+            blog_posts = BlogPost.objects.filter(publish=True, tags__in=[tag])
+        else:
+            blog_posts = BlogPost.objects.filter(publish=True)
+        return Response(BlogPostSerializer(blog_posts, many=True).data)
 
     model = BlogPost
     context_object_name = "blog_post_list"
 
-    def get_queryset(self):
-        tag_slug = self.kwargs.get("tag", "")
-        if tag_slug:
-            tag = get_object_or_404(Tag, slug=tag_slug)
-            queryset = BlogPost.objects.filter(publish=True, tags__in=[tag])
-        else:
-            queryset = BlogPost.objects.filter(publish=True)
-        return queryset
 
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(object_list=object_list, **kwargs)
-
-        tag_slug = self.kwargs.get("tag", "")
-        if tag_slug:
-            context["tag"] = get_object_or_404(Tag, slug=tag_slug)
-        context["tags"] = Tag.objects.all()
-
-        return context
-
-
-class BlogPostDetailView(DetailView):
+class BlogPostDetailView(APIView):
     """Detail view of a blog post."""
 
-    model = BlogPost
-    context_object_name = "blog_post"
-
-    def get_context_data(self, **kwargs):
-        blog_post = super().get_context_data(**kwargs)["blog_post"]
-        if not blog_post.publish:
+    def get(self, request, slug):
+        try:
+            blog_post = BlogPost.objects.get(slug=slug)
+            if not blog_post.publish:
+                raise Http404
+        except BlogPost.DoesNotExist:
             raise Http404
-
-        blog_post.paragraphs = BlogPostParagraph.objects.filter(
-            blog_post=self.get_object()
-        )
-        for paragraph in blog_post.paragraphs:
-            paragraph.snippets = BlogPostSnippet.objects.filter(paragraph=paragraph)
-
-        return {self.context_object_name: blog_post}
+        return Response(BlogPostSerializer(blog_post).data)
