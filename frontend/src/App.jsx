1bc1185e759aeaa485fe72d23e3deeafe1c0ea0c
Max Eriksson <teratoscincus@hotmail.com>
2026-05-01 14:03:59 +0200

    feat(core): migrate about template to React

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import { usePageTitle } from "./hooks/usePageTitle";
 import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
 
 function TemporaryPage({ title }) {
   usePageTitle(title);
@@ -16,7 +17,7 @@ function App() {
           <Route path="/" element={<HomePage title="Home" />} />
           <Route path="/portfolio/" element={<TemporaryPage title="Portfolio" />} />
           <Route path="/blog/" element={<TemporaryPage title="Blog" />} />
-          <Route path="/about/" element={<TemporaryPage title="About" />} />
+          <Route path="/about/" element={<AboutPage title="About" />} />
         </Route>
       </Routes>
     </BrowserRouter>
