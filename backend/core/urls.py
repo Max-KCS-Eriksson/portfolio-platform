9702d74eee080189b903828aba2ff86d5cafcfaf
Max Eriksson <teratoscincus@hotmail.com>
2026-05-01 12:37:24 +0200

    chore(context): move api to appropriate django app
    The API belongs to the same Django App that has its used context
    processor.

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import AboutView
+from .views import AboutView, FrontendContextView
 
 app_name = "core"
 urlpatterns = [
     path("about/", AboutView.as_view(), name="about"),
+    path("context/", FrontendContextView.as_view(), name="context"),
 ]
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,8 +1,9 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .context_processors import domain_name, site_owner, social_media_links
 from .models import About
-from .serializers import AboutSerializer
+from .serializers import AboutSerializer, SocialMediaLinkSerializer
 
 
 class AboutView(APIView):
@@ -12,3 +13,20 @@ class AboutView(APIView):
         except About.DoesNotExist:
             return Response({})
         return Response(AboutSerializer(about).data)
+
+
+class FrontendContextView(APIView):
+    """API for providing previously used template context processor injections."""
+
+    def get(self, request):
+        social_media_queryset = social_media_links(request)["social_media_links"]
+
+        return Response(
+            {
+                "domain_name": domain_name(request)["domain_name"],
+                "site_owner": site_owner(request)["site_owner"],
+                "social_media_links": SocialMediaLinkSerializer(
+                    social_media_queryset, many=True
+                ).data,
+            }
+        )
