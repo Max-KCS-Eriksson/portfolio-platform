62ba55069dc7a30fa0388cba2fc77b68ca6e9299
Max Eriksson <teratoscincus@hotmail.com>
2026-05-28 07:42:09 +0200

    feat(blog): add blog context model

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -6,6 +6,47 @@ from taggit.managers import TaggableManager
 from .utils import BlogMarkdownParseError, BlogUtility
 
 
+class BlogContext(models.Model):
+    """Context content for the blog overview page."""
+
+    featured = models.BooleanField(default=True)
+    intro = models.TextField()
+
+    def another_featured_context_exists(self):
+        return BlogContext.objects.exclude(pk=self.pk).filter(featured=True).exists()
+
+    def save(self, *args, **kwargs):
+        """Ensure only one instance is featured and that one always is featured."""
+        if self.featured:
+            BlogContext.objects.exclude(pk=self.pk).update(featured=False)
+        elif not self.another_featured_context_exists():
+            self.featured = True
+
+        return super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        """Promote another blog context if the featured one is deleted."""
+        was_featured = self.featured
+        result = super().delete(*args, **kwargs)
+
+        if not was_featured:
+            return result
+
+        if BlogContext.objects.filter(featured=True).exists():
+            return result
+
+        next_context = BlogContext.objects.order_by("-pk").first()
+
+        if next_context:
+            next_context.featured = True
+            next_context.save()
+
+        return result
+
+    def __str__(self):
+        return f"Blog context (v.{self.pk})"
+
+
 class BlogPost(models.Model):
     as_markdown = models.TextField(
         help_text="Write the blog post as Markdown, starting with a # title."
